74c20153b3d1889d6e06e9246a401dff75b52011
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-29 19:47:58 -0700

    nodemailer installed, back end post route with authorization set up

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const fetch = require("node-fetch");
+const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PASS = process.env.GMAIL_PASS;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -82,6 +88,40 @@ app.get("/portfolio", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact", { Title: "Contact" });
 });
+
+app.post('/contact', (req, res) => {
+  // Instantiate the SMTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    // secure defaults to port 465
+    port: 465,
+    secure: true,
+    // gmail credentials
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  })
+
+  // Specify what the email will look like
+  const mailOpts = {
+    from: "Your sender info here", // This is ignored by Gmail
+    to: GMAIL_USER,
+    subject: "You've got mail my dude!",
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  }
+
+  // Attempt to send the email
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.render('contact-failure') // Show a page indicating failure
+    }
+    else {
+      res.render('contact-success') // Show a page indicating success
+    }
+  })
+})
+
 
 app.listen(PORT, function () {
   console.log("Listening on http://localhost:" + PORT);
